247dd4c2fe966c19f78073f6d5fe56ed3e49a9cc
Cliffe Hodgkinson <cliffe@resounding.ca>
2012-11-19 22:59:59 -0500

    Initial commit

---
+++ Site/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Optimization;
+
+namespace Site
+{
+    public static class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/bundle")
+                .Include("~/css/bootstrap.css")
+                .Include("~/css/bootstrap-responsive.css")
+                .Include("~/css/app.css")
+            );
+
+            bundles.Add(new ScriptBundle("~/js/bundle")
+                .Include("~/js/jquery-1.8.3.js")
+                .Include("~/js/bootstrap.js")
+            );
+
+            // Code removed for clarity.
+            //BundleTable.EnableOptimizations = false;
+        }
+    }
+}
---
+++ Site/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Email(string address)
+        {
+            const string subject = "Someone is interested in Resounding Work";
+            var headers = string.Empty;
+            foreach (var header in Request.Headers.AllKeys) {
+                headers += "\t" + header + ": " + Request.Headers[header] + "\n";
+            }
+
+            var body = string.Format("Email: {0},\n\nHeaders: \n{1}", address, headers);
+            var fromAddress = new MailAddress("hodgkinson@gmail.com", "Cliffe Hodgkinson");
+            var toAddress = new MailAddress("cliffe@resounding.ca", "Cliffe Hodgkinson");
+
+            using (var smtp = new SmtpClient()) {
+                using (var message = new MailMessage(fromAddress, toAddress) {
+                    Subject = subject,
+                    Body = body
+                }) {
+                    smtp.Send(message);
+                }
+            }
+
+            return new EmptyResult();
+        }
+    }
+}
