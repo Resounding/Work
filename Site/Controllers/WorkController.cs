092b88aa4852261cc1ac88fedc41ce2d3069cb4a
Cliffe Hodgkinson <cliffe@resounding.ca>
2013-03-06 06:38:38 -0500

    Add crew chooser

---
+++ Site/Controllers/WorkController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Site.Models;
+
+namespace Site.Controllers
+{
+    public class WorkController : Controller
+    {
+        public ActionResult Index(Guid? crewId = null)
+        {
+            using (var context = new WorkOrderContext()) {
+                if (!(crewId.HasValue)) {
+                    var crews = context.Crews.OrderBy(c => c.Name).ToList();
+                    var model = new ChooseCrewModel { Crews = crews };
+
+                    return View("ChooseCrew", model);
+                }
+
+                return View();
+            }
+        }
+
+    }
+}
---
+++ Site/Models/ChooseCrewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+namespace Site.Models
+{
+    public class ChooseCrewModel
+    {
+        public IEnumerable<Crew> Crews { get; set; }
+    }
+}
